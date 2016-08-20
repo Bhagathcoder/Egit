d2fee942b9ce62d6a48c443853345ea9300660d3
bhagath <bhagath@bhagath-PC>
2016-08-19 23:17:24 -0700

    Change-Id: I8c3a84c5f8a5b7153d716a0ad0aadcc2fb6f16ca

---
+++ Inheritence/src/MainClass.java
@@ -0,0 +1,12 @@
+import com.bhagath.buisiness.Vehicle;
+
+public class MainClass {
+	public static void main(String args[]){
+		System.out.println("Hi");
+		Vehicle v=new Vehicle();
+		v.setEngine("V9");
+		System.out.println(v.getEngine());
+		
+	}
+
+}
---
+++ Inheritence/src/com/bhagath/buisiness/Vehicle.java
@@ -0,0 +1,21 @@
+package com.bhagath.buisiness;
+
+public class Vehicle {
+	
+	private String name="";
+	private String engine="";
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEngine() {
+		return engine;
+	}
+	public void setEngine(String engine) {
+		this.engine = engine;
+	}
+	
+
+}
